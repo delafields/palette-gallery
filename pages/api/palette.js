bc74b4bf0679472cba9ddf0d8f7ccd758d096e8d
Jeremy Fields <j.c.delafields@gmail.com>
2024-04-23 18:57:20 -0400

    initial commit

---
+++ pages/api/palette.js
@@ -0,0 +1,40 @@
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+export default async function fetchPalette (req, res) {
+    let { hex } = req.query
+    console.log('hex:', hex)
+
+    const options = {
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    };
+    const browser = await puppeteer.launch(options);
+
+    const page = await browser.newPage()
+
+    const url = 'https://artsexperiments.withgoogle.com/artpalette/colors/' + hex
+
+    await page.goto(url, { waitUntil: 'networkidle0' });
+
+    // scrape images
+    const els = await page.$$('div.result-item');
+
+    const images = [];
+
+    for (let i = 0; i < els.length; i++) {
+        let url = await els[i].$eval('div.item-image > img', i => i.getAttribute('src'));
+        // grab a larger version of the image
+            url = url.replace(/=w(\d)+/, '=w1000')
+        // console.log(img);
+        let title = await els[i].$eval('a.more-details', a => a.getAttribute('title'));
+              title = title.replace(/<\/?i>/g, '')
+        // console.log(link);
+
+        images.push({"title": title, "url": url})
+    }
+    
+    // console.log(images)
+    return res.status(200).json( images );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,233 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Head from 'next/head'
+// import Image from 'next/image'
+import Image from "next/legacy/image";
+import React, { useState, useEffect, useCallback } from "react";
+import { HexColorPicker } from "react-colorful";
 
-const inter = Inter({ subsets: ["latin"] });
+const ColorPicker = ({ index, color, onColorChange, generateHexCode }) => {
+  const [showPicker, setShowPicker] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Set isClient to true once the component mounts
+    setIsClient(true);
+  }, []);
+
+  const handleColorChange = (newColor) => {
+    onColorChange(index, newColor);
+  };
+
+  // Only apply dynamic styles if isClient is true
+  const dynamicStyle = isClient ? { backgroundColor: color } : {};
+
+  return (
+    <div className="relative w-1/6">
+      {color ? (
+        <div
+          className="relative h-[150px] bg-gray-300 cursor-pointer"
+          style={dynamicStyle}
+          onMouseEnter={() => setShowPicker(true)}
+          onMouseLeave={() => setShowPicker(false)}
+        >
+          {showPicker && (
+            <div className="absolute z-10 w-full">
+              {index != 0 ? (
+                <button
+                  className="absolute w-full -translate-y-6 align-center"
+                  onClick={() => handleColorChange(null)}
+                >
+                  🗑️
+                </button>
+              ) : null}
+              <HexColorPicker
+                style={{ width: "auto", height: "140px" }}
+                color={color}
+                onChange={handleColorChange}
+              />
+            </div>
+          )}
+        </div>
+      ) : (
+        <button
+          style={dynamicStyle}
+          className="w-full h-full text-3xl font-extrabold text-black border-l-2"
+          onClick={() => handleColorChange(generateHexCode())}
+        >
+          +
+        </button>
+      )}
+    </div>
+  );
+};
+
+
+const ImageCarousel = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const prevImageIndex = currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1;
+  const nextImageIndex = currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1;
+
+  const handlePrevClick = useCallback(() => {
+    setCurrentImageIndex(prevImageIndex);
+  }, [prevImageIndex]);
+
+  const handleNextClick = useCallback(() => {
+    setCurrentImageIndex(nextImageIndex);
+  }, [nextImageIndex]);
+
+  const handleKeyDown = useCallback(
+    (e) => {
+      if (e.key === 'ArrowLeft') {
+        handlePrevClick();
+      } else if (e.key === 'ArrowRight') {
+        handleNextClick();
+      }
+    },
+    [handlePrevClick, handleNextClick]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+  
+  // picture frame: https://codepen.io/chris22smith/pen/PbBwjp
+  return (
+    <>
+      <div className="relative w-11/12 mt-4 h-5/6">
+        {images.map((image, index) => (
+            <Image
+              key={index}
+              src={image.url}
+              alt="image"
+              layout="fill"
+              objectFit="contain"
+              className={`absolute drop-shadow-[0_3px_10px_rgb(0,0,0,0.2)] top-0 left-0 transition-opacity duration-500 ${
+                index === currentImageIndex ? 'opacity-100' : 'opacity-0'
+              }`}
+            />
+
+        ))}
+      </div>
+
+      {images[currentImageIndex].title
+        ? <div className='absolute bottom-0 flex items-center justify-center w-64 h-24 p-4 mb-10 bg-white right-10 drop-shadow-md'>
+            <p className='font-bold text-black'>{images[currentImageIndex].title}</p>
+          </div>
+        : <></>
+      }
+
+      </>
+  );
+};
+
+
 
 export default function Home() {
+  const [images, setImages] = useState(null);
+  const [colors, setColors] = useState(Array(5).fill(null).map(generateHexCode));
+
+  const handleColorChange = (index, newColor) => {
+    setColors((prevColors) => {
+      const newColors = [...prevColors];
+      newColors[index] = newColor;
+      return newColors;
+    });
+  };
+
+  function generateHexCode() {
+    const hexChars = '0123456789ABCDEF';
+    let hex = '';
+  
+    // Generate a random 6-digit hex code
+    for (let i = 0; i < 6; i++) {
+      hex += hexChars[Math.floor(Math.random() * 16)];
+    }
+    
+    // console.log(hex)
+    hex = "#" + hex.toLowerCase()
+    return hex;
+  }
+  
+  const generateRandomPalette = () => {
+    const numColors = Math.floor(Math.random() * 4) + 2;
+    const newColors = Array(5).fill(null);
+  
+    for (let i = 0; i < numColors; i++) {
+      newColors[i] = generateHexCode();
+    }
+  
+    setColors(newColors);
+
+    fetchPalette();
+  };
+
+  async function fetchPalette() {
+    // const res = await fetch("/api/palette?hex=374121-5d4538-477e92-c0b69e-163a60");
+    let url = `/api/palette?hex=${colors[1]}-${colors[2]}-${colors[3]}-${colors[4]}-${colors[5]}`
+        url = url.replaceAll('#', '').replaceAll('-undefined', '').replaceAll('-null', '')
+    
+    console.log('fetching new palette')
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error(`Failed to fetch palette: ${res.statusText}`);
+      }
+      const data = await res.json();
+      setImages(data);
+      console.log('received and set new images')
+    } catch (error) {
+      console.error('Error fetching new palette:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPalette();
+  }, []);
+
+  
+
+  
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Generated by create next app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col h-screen bg-amber-50 bg-fuzz">
+
+        <div className="relative flex justify-center w-full h-full">
+          {images 
+            ? <ImageCarousel images={images} />
+            : <p>Loading....</p>
+          }
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className='flex w-full drop-shadow-[0_-3px_3px_rgba(0,0,0,0.1)]'>
+            
+            {colors.map((color, index) => (
+              <ColorPicker 
+                key={index} 
+                index={index} 
+                color={color} 
+                onColorChange={handleColorChange}
+                generateHexCode={generateHexCode}
+              />
+            ))}
+            
+            <div className='flex flex-col justify-center h-full gap-4 bg-black border-b-2 border-l-2 border-r-2 grow'>
+              <button className='px-2 py-1 font-bold text-white rounded-lg hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-amber-200 to-yellow-400' onClick={generateRandomPalette}>RANDOM</button>
+              {/* <button className='px-2 py-1 font-bold text-white rounded-lg hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-amber-200 to-yellow-400' onClick={fetchPalette}>FETCH</button> */}
+            </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      
+      </main>
+    </>
+  )
 }
